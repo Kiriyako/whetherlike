cce2a8db132753d4096a79c5e53da44bd7f1f359
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2022-12-10 16:03:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ let weather = {
      const { icon, description } = data.weather[0];
      const { temp, humidity } = data.main;
      const { speed } = data.wind;
-     document.querySelector(".city").innerText = "Weather in" + " " +  name;
+     const {country} = data.sys;
+     document.querySelector(".city").innerText = "Weather in" + " " +  name + "," +" " + country;
      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png"
      document.querySelector(".description").innerText = description;
-     document.querySelector(".temp  ").innerText = temp + "°C";
+     document.querySelector(".temp").innerText = temp + "°C";
+     document.querySelector(".country").innerText = country; 
      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
      document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
      document.querySelector(".weather").classList.remove("loading");
